05d27945f2df0ad16e23d45d8aa1f60bce568078
nothingcosmos <nothingcosmos@gmail.com>
2016-12-22 20:16:12 +0900

    add release build

--- work/build.sh
+++ work/build.sh
@@ -3,6 +3,7 @@ pushd ../proto-quic
 export PROTO_QUIC_ROOT=`pwd`
 export PATH=${PROTO_QUIC_ROOT}/depot_tools:$PATH
 outpath="out/Release"
+gnopt="--args=is_debug=false"
 list="quic_client quic_server epoll_quic_client epoll_quic_server"
 
 if [ ! -f proto_quic_tools/sync ] ; then
@@ -12,7 +13,7 @@ fi
 
 pushd ${PROTO_QUIC_ROOT}/src
 for out in $outpath; do
-  gn gen $out && ninja -C $out $list
+  gn gen $out $gnopt && ninja -C $out $list
 done
 popd
 popd
